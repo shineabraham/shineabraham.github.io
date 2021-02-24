56862df04b421254ce0625609642e831aff53e61
Shine Abraham <shineabraham@Shines-MacBook-Pro.local>
2021-02-24 11:12:18 +0530

    added data to resume and changes in image size

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -18,12 +18,23 @@ class Resume extends Component {
       var education = this.props.data.education.map(function(education){
         return <div key={education.school}><h3>{education.school}</h3>
         <p className="info">{education.degree} <span>&bull;</span><em className="date">{education.graduated}</em></p>
-        <p>{education.description}</p></div>
+        <p>{education.description2}</p>
+        <p>{education.description}</p>
+        <p>{education.description1}</p>
+        
+        
+        </div>
       })
       var work = this.props.data.work.map(function(work){
         return <div key={work.company}><h3>{work.company}</h3>
             <p className="info">{work.title}<span>&bull;</span> <em className="date">{work.years}</em></p>
             <p>{work.description}</p>
+            <p>{work.description1}</p>
+            <p>{work.description2}</p>
+            <p>{work.description3}</p>
+            <p>{work.description4}</p>
+            <p>{work.description5}</p>
+            <p>{work.description6}</p>
         </div>
       })
 
