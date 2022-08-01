644ec2eea70bf4b9c3852d7c5cc08b3856e887e5
Philson K Philip <philson.k.philip@gmail.com>
2022-08-01 11:21:29 +0530

    Updated the site

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -23,48 +23,6 @@ class Header extends Component {
     return (
       <header id="home">
         <ParticlesBg type="circle" bg={true} />
-        <nav id="nav-wrap">
-          <a className="mobile-btn" href="#nav-wrap" title="Show navigation">
-            Show navigation
-          </a>
-          <a className="mobile-btn" href="#home" title="Hide navigation">
-            Hide navigation
-          </a>
-
-          <ul id="nav" className="nav">
-            <li className="current">
-              <a className="smoothscroll" href="#home">
-                Home
-              </a>
-            </li>
-            <li>
-              <a className="smoothscroll" href="#about">
-                About
-              </a>
-            </li>
-            <li>
-              <a className="smoothscroll" href="#resume">
-                Resume
-              </a>
-            </li>
-            <li>
-              <a className="smoothscroll" href="#portfolio">
-                Works
-              </a>
-            </li>
-            <li>
-              <a className="smoothscroll" href="#blog">
-                Read
-              </a>
-            </li>
-            <li>
-              <a className="smoothscroll" href="#contact">
-                Contact
-              </a>
-            </li>
-          </ul>
-        </nav>
-
         <div className="row banner">
           <div className="banner-text">
             <h1 className="responsive-headline">{name}</h1>
